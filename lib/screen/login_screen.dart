38b8cf78d277f56cc41add706fa850d9f73df3b7
Kennedy Chiang <kennedychiang@fugle.tw>
2021-07-17 21:13:01 +0800

    lib/screen/login_screen.dart update ui layout and login feature

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:test_app/bloc/auth/auth_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -10,8 +13,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('1-Person Twitter')),
-      body: _body,
+      body: SafeArea(child: _body),
     );
   }
 
@@ -19,11 +21,35 @@ class _LoginScreenState extends State<LoginScreen> {
   ///
 
   Widget get _body {
-    return Center(
-      child: ElevatedButton(
-        child: Text('Google SignIn'),
-        onPressed: () {},
-      ),
+    return Column(
+      children: [
+        Container(
+          constraints: BoxConstraints.tightFor(
+            width: double.infinity,
+            height: kToolbarHeight,
+          ),
+          child: Center(
+            child: FaIcon(
+              FontAwesomeIcons.twitter,
+              color: Colors.blue,
+              size: 28.0,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Center(
+            child: ElevatedButton.icon(
+              icon: FaIcon(
+                FontAwesomeIcons.googlePlusG,
+              ),
+              label: Text('SignIn'),
+              onPressed: () {
+                BlocProvider.of<AuthBloc>(context).add(GoogleSignInEvent());
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
