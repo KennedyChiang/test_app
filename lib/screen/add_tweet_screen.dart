654b910b8e59dff3cfe0cd7a57890cb418fab9a1
Kennedy Chiang <kennedychiang@fugle.tw>
2021-07-17 21:58:58 +0800

    add new screen lib/screen/add_tweet_screen.dart

---
+++ lib/screen/add_tweet_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:test_app/bloc/auth/auth_bloc.dart';
+
+const String _defaultAvatarPath = 'assets/images/icon-ninja-13.jpg';
+
+class AddTweetScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AddTweetScreenState();
+}
+
+class _AddTweetScreenState extends State<AddTweetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar,
+      body: _body,
+    );
+  }
+
+  /// getter
+  ///
+
+  AppBar get _appBar => AppBar(
+        leading: IconButton(
+          icon: Text('Cancel'),
+          onPressed: () {},
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Tweet'),
+          ),
+        ],
+      );
+
+  Widget get _body {
+    return Center(
+      child: Text('Add Tweet Screen'),
+    );
+  }
+
+  Widget get drawer => Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 60.0,
+                    height: 60.0,
+                    child: null == BlocProvider.of<AuthBloc>(context).photoUrl
+                        ? Image.asset(
+                            _defaultAvatarPath,
+                            fit: BoxFit.cover,
+                          )
+                        : FadeInImage(
+                            image: NetworkImage(
+                              BlocProvider.of<AuthBloc>(context).photoUrl!,
+                            ),
+                            placeholder: const AssetImage(_defaultAvatarPath),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                  Text(BlocProvider.of<AuthBloc>(context).googleId ?? '-'),
+                  Text(BlocProvider.of<AuthBloc>(context).email ?? '-'),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text('SignOut'),
+              onTap: () {
+                BlocProvider.of<AuthBloc>(context).add(GoogleSignOutEvent());
+              },
+            ),
+          ],
+        ),
+      );
+}
