81396dedbef1dbb57db36be44475a4264527d2bd
Kennedy Chiang <kennedychiang@fugle.tw>
2021-07-17 22:35:47 +0800

    update lib/model/google_auth.dart implement

---
+++ lib/model/google_auth.dart
@@ -0,0 +1,34 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+Future<GoogleAuthInfo> googleAuth() async {
+  GoogleSignIn googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+    ],
+  );
+  final result = await googleSignIn.signIn();
+  GoogleSignInAuthentication authentication = await result!.authentication;
+
+  return GoogleAuthInfo(
+    displayName: result.displayName,
+    email: result.email,
+    id: result.id,
+    photoUrl: result.photoUrl,
+    authentication: authentication,
+  );
+}
+
+class GoogleAuthInfo {
+  final String? displayName;
+  final String email;
+  final String id;
+  final String? photoUrl;
+  final GoogleSignInAuthentication authentication;
+  GoogleAuthInfo({
+    required this.displayName,
+    required this.email,
+    required this.id,
+    required this.photoUrl,
+    required this.authentication,
+  });
+}
