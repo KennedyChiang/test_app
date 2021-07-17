a60279cc3664ae1d1698a104a3791a76623e3819
Kennedy Chiang <kennedychiang@fugle.tw>
2021-07-17 21:12:01 +0800

    update lib/main.dart implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_app/bloc/auth/auth_bloc.dart';
 import 'package:test_app/screen/login_screen.dart';
+import 'package:test_app/screen/twitter_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +19,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider<AuthBloc>(
+        create: (_) => AuthBloc(),
+        child: Builder(
+          builder: (providerContext) => BlocListener<AuthBloc, AuthState>(
+            bloc: BlocProvider.of<AuthBloc>(providerContext),
+            listener: (authContext, authState) {
+              print('BlocListener got authState: $authState');
+            },
+            child: BlocBuilder<AuthBloc, AuthState>(
+              bloc: BlocProvider.of<AuthBloc>(providerContext),
+              builder: (authContext, authState) {
+                if (authState is AuthInitial) {}
+                if (authState is NoneAuthState) {}
+                if (authState is GoogleSignInSuccessState) {
+                  return TwitterScreen();
+                }
+                return LoginScreen();
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
