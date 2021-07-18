92c1d02667ce431155ed56d9145a2327ac138be9
Kennedy Chiang <kennedychiang@fugle.tw>
2021-07-18 14:14:50 +0800

    update lib/model/tweet.dart

--- lib/model/tweet.dart
+++ lib/model/tweet.dart
@@ -2,4 +2,6 @@ class Tweet {
   final DateTime time;
   final String content;
   Tweet({required this.time, required this.content});
+
+  String get refKey => this.time.microsecondsSinceEpoch.toString();
 }
